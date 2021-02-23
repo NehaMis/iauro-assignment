8e9a48d12ac45488adfd2cbeceacb348b1f75eb8
Zeeshan Danish <zeeshan.danish@vendekin.com>
2021-02-23 12:50:40 +0530

    table view, student details, add, delete, update

---
+++ src/localstorage.js
@@ -0,0 +1,51 @@
+/* set registration data into local storage */
+export function setData(data) {
+  const arrStudentData = getData();
+  arrStudentData.push(data);
+  localStorage.setItem("users", JSON.stringify(arrStudentData));
+}
+
+/* update data into local storage by index */
+export function updateData(index, newData) {
+  const arrStudentData = getData();
+
+  arrStudentData[index] = newData;
+
+  localStorage.setItem("users", JSON.stringify(arrStudentData));
+}
+
+/* set page type need for local storage data update
+used in registration for reload window page and show data it to list  */
+export function setPageType(pageType) {
+  localStorage.setItem("page", pageType);
+}
+
+/* retrieve page type form local storage */
+export function getPageType() {
+  return localStorage.getItem("page");
+}
+
+/* get rows of data from local storage */
+export function getData() {
+  return JSON.parse(localStorage.getItem("users")) || [];
+}
+/* delete register user form local storage */
+export function deleteData(deletingData) {
+  const arrStudentData = getData() || [];
+  const data = arrStudentData.filter(studentData => {
+    return (
+      studentData.name === deletingData.name &&
+      deletingData.age === studentData.age &&
+      studentData.number === deletingData.number
+    );
+  });
+
+  if (data.length > 0) {
+    const index = arrStudentData.indexOf(data[0]);
+    if (index > -1) {
+      arrStudentData.splice(index, 1);
+    }
+  }
+
+  localStorage.setItem("users", JSON.stringify(arrStudentData));
+}
